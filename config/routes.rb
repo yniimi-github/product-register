0141570d28ef05a67641ad0a53f5c0707d1fabe4
yniimi-github <y.niimi.jobs@gmail.com>
2020-10-11 12:53:09 +0000

    devise create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'products#index'
+  devise_for :users
+  root 'pages#index'
+  get 'pages/show'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
