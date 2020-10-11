10ffbebdab374954f87e25eacd185d4bb6d0b880
yniimi-github <y.niimi.jobs@gmail.com>
2020-10-12 00:53:19 +0900

    test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # default url
-  config.action_mailer.default_url_options = { host: 'example.com'}
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "tt4613535@gmail.com",
+    :password => "passtt4613535",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
