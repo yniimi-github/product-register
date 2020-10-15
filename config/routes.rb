af200a78bc81eb0ba82c83098fde6e3ffa4f5a98
yniimi-github <y.niimi.jobs@gmail.com>
2020-10-15 22:34:51 +0900

    レイアウトの簡易的な修正・ルートの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'rooms#show'
+  root to: 'pages#index'
   devise_for :users
   get 'pages/index'
   get 'pages/show'
+  get 'rooms/show'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
